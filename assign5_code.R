b4860b02d8e958e78ad4eada3bdf8ed0cd4ab78a
andrewbernier <andrew.g.bernier@gmail.com>
2021-05-04 17:02:20 -0500

    done through step 2

--- assign5_code.R
+++ assign5_code.R
@@ -21,6 +21,42 @@ efcode.att.f
 efcode.attmat.f
 
 
+task.mat <- respondent_df %>%
+  select(screen, RAM, processor, price, brand) %>%
+  as.matrix()
+
+X.mat <- efcode.attmat.f(task.mat)
+
+dim(task.mat)
+dim(X.mat)
+
+head(task.mat)
+
+# In X.mat, cols 1&2 are for the screen variable
+# cols 3&4 = RAM, cols 5&6 = processor
+# cols 7&8 = price, cols 9-11 = brand
+# A value of 0 for an attribute in task.mat = -1's for all corresponding
+# columns in X.mat
+head(X.mat)
+
+pricevec <- (respondent_df %>% pull(price)) - mean(respondent_df$price)
+
+X.brands <- X.mat[,9:11]
+X.BrandByPrice <- X.brands*pricevec
+
+X.matrix <- cbind(X.mat, X.BrandByPrice)
+
+ydata <- resp.data.v3 %>%
+  select(starts_with("DCM1_"), -dcm1_timer) %>% 
+  as.matrix()
+
+sum(is.na(ydata))
+
+zowner <- 1 * (!is.na(resp.data.v3$vList3))
+
+#####################################
+# This is the end of step 2
+#####################################
 
 
 
@@ -29,5 +65,6 @@ efcode.attmat.f
 
 
 
-rhierMnlDP()
+
+
 
